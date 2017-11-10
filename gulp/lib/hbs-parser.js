77a8c15568259dde07d9521b77d20852f23d42a7
alxbenz <benzalexander@gmx.de>
2017-11-10 17:24:40 +0100

    disabled debugging

--- gulp/lib/hbs-parser.js
+++ gulp/lib/hbs-parser.js
@@ -2,7 +2,7 @@ const hbsHelpers = require('./hbs-helpers');
 const hb = require('gulp-hb');
 const fm = require('front-matter');
 
-const createHbsGulpStream = (partials, dataObject, dataGlob, debug=true) => {
+const createHbsGulpStream = (partials, dataObject, dataGlob, debug=false) => {
 	// @TODO add possibility to inject helpers from external file
 
 	let hbStream = hb({ debug: debug })
